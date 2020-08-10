eab66ac135c8cbc5e574877f0bf2df0e175fd608
Ilmiyatus <42805955+Ilmiyatus@users.noreply.github.com>
2020-08-10 09:06:54 +0700

    Update Test

--- Test.php
+++ Test.php
@@ -4,6 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 require_once "Employee.php";
 require_once "Wordcount.php";
+require_once "Stack.php";
 
 class Test extends TestCase
 {
@@ -13,16 +14,19 @@ class Test extends TestCase
         $id = 1;
         $name = 'Ilmiyatus Sadiah';
         $basicSalary = 10000000;
+        $date = '17/09/99';
 
         $obj = new Employee(
             $id,
             $name,
-            $basicSalary
+            $basicSalary,
+            $date
         );
 
         $this->assertEquals($id, $obj->getId());
         $this->assertEquals($name, $obj->getName());
         $this->assertEquals($basicSalary, $obj->getBasicSalary());
+        $this->assertEquals($date, $obj->getDate());
     }
 
     public function testCountWords()
@@ -33,5 +37,18 @@ class Test extends TestCase
         $WordCount = $Wc->countWords($TestSentence);
 
         $this->assertEquals(5, $WordCount); 
+    }
+
+    public function testPushAndPop()
+    {
+        $stack = [];
+        $this->assertSame(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertSame('foo', $stack[count($stack)-1]);
+        $this->assertSame(1, count($stack));
+
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertSame(0, count($stack));
     }
 }
